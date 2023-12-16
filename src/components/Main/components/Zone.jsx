68f29ae053729ed057e05f73a7b14b1378e414d8
강동욱 <ehddnr8563@gmail.com>
2023-12-17 03:20:14 +0900

    add: ui 로직 변경

--- src/components/Main/components/Zone.jsx
+++ src/components/Main/components/Zone.jsx
@@ -3,62 +3,13 @@ import styles from './Zone.module.css';
 import List from './List';
 
 export default function Zone({ taskState }) {
-  if (taskState === 'wait')
-    return (
-      <li className={styles.menu__wait}>
-        <div className={styles.menu__title}>
-          <h2>대기중</h2>
-          <button>﹢</button>
-        </div>
-        <List />
-      </li>
-    );
-
-  if (taskState === 'progress') {
-    return (
-      <li className={styles.menu__progress}>
-        <div className={styles.menu__title}>
-          <h2>진행중</h2>
-          <button>﹢</button>
-        </div>
-        <List />
-      </li>
-    );
-  }
-
-  if (taskState === 'done') {
-    return (
-      <li className={styles.menu__done}>
-        <div className={styles.menu__title}>
-          <h2>완료</h2>
-          <button>﹢</button>
-        </div>
-        <List />
-      </li>
-    );
-  }
-
-  if (taskState === 'issue') {
-    return (
-      <li className={styles.menu__issue}>
-        <div className={styles.menu__title}>
-          <h2>이슈</h2>
-          <button>﹢</button>
-        </div>
-        <List />
-      </li>
-    );
-  }
-
-  if (taskState === 'hold') {
-    return (
-      <li className={styles.menu__hold}>
-        <div className={styles.menu__title}>
-          <h2>홀드</h2>
-          <button>﹢</button>
-        </div>
-        <List />
-      </li>
-    );
-  }
+  return (
+    <li className={styles[taskState]}>
+      <div className={styles.title}>
+        <h2>{taskState}</h2>
+        <button>﹢</button>
+      </div>
+      <List />
+    </li>
+  );
 }
